2710165651f80a8b481d74511c08546bf3f80b47
Robbie Kelly <robbiekelly@Robbies-MacBook-Air-2.local>
2025-03-11 08:35:35 -0400

    add enumerate/for loop

---
+++ character_indexer.py
@@ -0,0 +1,14 @@
+#Given a string, find the first non-repeating character and return its index. If all characters repeat, return -1.
+
+#import applicable modules
+from collections import counter
+
+Uinput = input("Enter a single word. ")
+
+#need to take string and and count how often characters appear
+    #how does python count characters
+counter = counter(Uinput)
+#find the first one that only occurs once
+    #how does pythong check the order?
+def enumerate
+#return -1 if no unique characters
